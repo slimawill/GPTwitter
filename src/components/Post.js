f6567a3f00b67a635f228fb7b9d9b660ff2e0cf8
William Lima <williamlima142014@gmail.com>
2023-06-03 23:38:18 -0300

    declared initial post layout

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,7 @@
 function Post(props) {
-    return <div className="Post">
-        <h1>{props.author}</h1>
-        <p>{props.msg}</p>
+    return <div className="text-black mx-auto text-center overflow-hidden m-4 rounded-full border-white border-4 oitenta">
+        <h1 className="bg-white">{props.author}</h1>
+        <p className="h-auto">{props.msg}</p>
     </div>
 }
 
